da6ffc772203f338b42aeb9b7a85dc9b5ea71816
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-06-03 17:06:18 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,11 +1,12 @@
-
+require 'pry'
 
 class Application < Sinatra::Base
    get '/' do
     erb :index
   end
     post '/greet' do
-      @name = params["user_name"] 
+      @name = params["user_name"]
+      binding.pry
     erb :greet
   end
 
